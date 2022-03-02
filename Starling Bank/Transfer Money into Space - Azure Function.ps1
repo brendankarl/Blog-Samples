afbaef8240a2eff54566111157ec0164d8cbef29
Brendan Griffin <brendankgriffin@hotmail.com>
2022-03-02 15:25:29 +0000

    Update Transfer Money into Space - Azure Function.ps1

--- Starling Bank/Transfer Money into Space - Azure Function.ps1
+++ Starling Bank/Transfer Money into Space - Azure Function.ps1
@@ -3,18 +3,18 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-$PAT = $Env:PAT 
+$PAT = $Env:PAT # The Personal Access Token (PAT) is stored as an Application Setting (named "PAT") within the function app
 $Headers = @{Authorization="Bearer " + $PAT} 
 $BaseURL = "https://api.starlingbank.com/api/v2/"
 
-# Retrieve default account uid
+# Retrieve default account uid (I'm being lazy, I have a single account)
 function Get-AccountUID {
     $Response = Invoke-WebRequest -Uri ($BaseURL + "accounts") -Method Get -Headers $Headers
     $AccountUID = $Response | ConvertFrom-Json
     return $AccountUID.accounts[0].accountUid
 
 }
-# Retrieve the space uid for the first space (I'm being lazy.....I have a single space)
+# Retrieve the space uid for the first space (I'm being lazy, I have a single Savings Space)
 function Get-SpaceUID {
     $Response = Invoke-WebRequest -Uri ($BaseURL + "account" + "/" + (Get-AccountUID) + "/savings-goals") -Method Get -Headers $Headers
     $Spaces = $Response | ConvertFrom-Json
@@ -26,7 +26,7 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 # If the merchant looks like McDonalds then invoke the burger tax!
 if ($Request.body.content.counterPartyName -like "mcdonalds*")
     {
-        Write-Host "Invoking burger tax..."
+        Write-Host "You spent money at " $($Request.body.content.counterPartyName) ", invoking burger tax..."
         # Calculate the burger tax - this is 20% of the transaction amount (rounded up to the nearest penny)
         $TaxAmount = [math]::ceiling($Request.body.content.amount.minorUnits * 0.20)
         $Body = "Burger tax:" + " " + "£" + ($TaxAmount /100)
@@ -43,10 +43,11 @@ if ($Request.body.content.counterPartyName -like "mcdonalds*")
         # Transfer the burger tax calculated to the space returned by Get-SpaceUID
         $Transfer = Invoke-WebRequest -Uri ($BaseURL + "account/" + (Get-AccountUID) + "/savings-goals/" + (Get-SpaceUID) + "/add-money/" + ((New-Guid).Guid)) -Method Put -ContentType "application/json" -Headers $Headers -Body $JSON
     }
+# It's not McDonalds.....so all is good!
 else 
     {
-        $Body = "Good Boy!"
-        Write-Host "Good Boy!"
+        $Body = "You spent money at " + $($Request.body.content.counterPartyName)
+        Write-Host $Body
     }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.

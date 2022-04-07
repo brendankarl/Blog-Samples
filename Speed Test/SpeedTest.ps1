2a51889dd070919536537a0cd4d79dcdc843544f
Brendan Griffin <brendankgriffin@hotmail.com>
2022-04-07 16:25:42 +0100

    Add files via upload

---
+++ Speed Test/SpeedTest.ps1
@@ -0,0 +1,8 @@
+$i = 0
+while ($i -eq 0)
+{
+    $Time = Get-Date
+    $SpeedTest = speedtest-cli --simple
+    $Time.ToString() + "," + $SpeedTest[0].split(" ")[1] + "," + $SpeedTest[1].split(" ")[1] + "," + $SpeedTest[2].split(" ")[1]  >> ("/etc/speedtest/logs/SpeedTest-" + $Time.DayOfYear + ".csv")
+    Start-Sleep -Seconds 300
+}

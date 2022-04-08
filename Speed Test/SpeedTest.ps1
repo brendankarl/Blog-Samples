371f783cbe6b136bbcdb6cbe60be394230f54bed
Brendan Griffin <brendankgriffin@hotmail.com>
2022-04-08 10:01:15 +0100

    Update SpeedTest.ps1

--- Speed Test/SpeedTest.ps1
+++ Speed Test/SpeedTest.ps1
@@ -3,6 +3,6 @@ while ($i -eq 0)
 {
     $Time = Get-Date
     $SpeedTest = speedtest-cli --simple
-    $Time.ToString() + "," + $SpeedTest[0].split(" ")[1] + "," + $SpeedTest[1].split(" ")[1] + "," + $SpeedTest[2].split(" ")[1]  >> ("/etc/speedtest/logs/SpeedTest-" + $Time.DayOfYear + ".csv")
+    $Time.ToString() + "," + $SpeedTest[0].split(" ")[1] + "," + $SpeedTest[1].split(" ")[1] + "," + $SpeedTest[2].split(" ")[1]  >> "/etc/speedtest/logs/SpeedTest.csv"
     Start-Sleep -Seconds 300
 }

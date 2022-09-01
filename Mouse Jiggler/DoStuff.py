eeb20bb6b9f3971f6235005b05ddb4e5b8023952
Brendan Griffin <brendankgriffin@hotmail.com>
2022-09-01 15:19:23 +0100

    Create DoStuff.py

---
+++ Mouse Jiggler/DoStuff.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+while True:
+    pyautogui.click(x=100,y=100)
+    pyautogui.press('enter')
+    time.sleep(5)
+    pyautogui.click(x=200,y=200)
+    pyautogui.press('enter')
+    time.sleep(5)

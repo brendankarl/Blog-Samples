0eedffb99a759bc0f422af654a41452270b99821
Brendan Griffin <brendankgriffin@hotmail.com>
2024-02-19 14:40:12 +0000

    Update SpeechToText.py

--- OCI/SpeechToText.py
+++ OCI/SpeechToText.py
@@ -11,13 +11,6 @@ transcriptJSON = json.load(transcriptsource)
 transcriptJSON["transcriptions"][0]["transcription"] # This is looking at the output of the first transciption within the output, denoted by 0. If transcribing multiple videos within a single job, this would need to be updated accordingly.
 
 for word in transcriptJSON["transcriptions"][0]["tokens"]:
-    if word["confidence"] < '0.70':
-        if word["token"] not in ('.',','):
-            print(word["token"] + " - " + "Confidence: " + word["confidence"] + " at: " + word["startTime"])
-
-
-for word in transcriptJSON["transcriptions"][0]["tokens"]:
     if word["confidence"] < '0.70': # Only display words with a detection confidence of less than 70%
         if word["token"] not in ('.',','): # Exclude full stops and commas
             print(word["token"] + " - " + "Confidence: " + word["confidence"] + " at: " + word["startTime"])
-
